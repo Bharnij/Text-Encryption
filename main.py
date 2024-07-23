7637bb9d09ab3338311a6d7cb2e7ee70b53336cb
Bharnij M Kashyap <130507731+Bharnij@users.noreply.github.com>
2024-07-23 11:39:53 +0530

    added ASCII art

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
 
-################################
+print('''
+
+  ____ _       _                 _____                             _     
+ / ___(_)_ __ | |__   ___ _ __  | ____|_ __   ___ _ __ _   _ _ __ | |_   
+| |   | | '_ \| '_ \ / _ \ '__| |  _| | '_ \ / __| '__| | | | '_ \| __|  
+| |___| | |_) | | | |  __/ |    | |___| | | | (__| |  | |_| | |_) | |_ _ 
+ \____|_| .__/|_| |_|\___|_|    |_____|_| |_|\___|_|   \__, | .__/ \__(_)
+        |_|                                            |___/|_|          
+
+
+''')
+
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -21,15 +32,12 @@ def encrypt(plain_text, shift_amount):
         if letter in alphabet:
             
             position = alphabet.index(letter)
-            
-            new_position = (position + shift_amount) % 26  
-            
+            new_position = (position + shift_amount) % 26              
             new_letter = alphabet[new_position]
             
             cipher_text += new_letter
         
-        else:
-            
+        else:            
             cipher_text += letter  
     
     print(f"The encoded message is {cipher_text}")
@@ -42,33 +50,26 @@ def decrypt(cipher_text, shift_amount):
     for letter in cipher_text:
     
         if letter in alphabet:
-    
-            position = alphabet.index(letter)
-    
-            new_position = (position - shift_amount) % 26  
-    
+            position = alphabet.index(letter)    
+         
+            new_position = (position - shift_amount) % 26      
             plain_text += alphabet[new_position]
     
         else:
-    
             plain_text += letter  
-    
+
     print(f"The decoded message is {plain_text}")
 
 
 if direction == "encode":
-
     encrypt(plain_text=text, shift_amount=shift)
 
 elif direction == "decode":
-
     decrypt(cipher_text=text, shift_amount=shift)
 
 else:
-
     print("Invalid input. Please type 'encode' to encrypt or 'decode' to decrypt.")
 
 
-####################################
 
     
